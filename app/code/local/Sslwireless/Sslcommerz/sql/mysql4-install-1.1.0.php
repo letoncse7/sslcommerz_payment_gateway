3830ddc2f1cecd759eec1ee9b612194d4123ee3a
Leton <letoncse7@gmail.com>
2016-04-20 09:56:08 +0600

    Final commit

---
+++ app/code/local/Sslwireless/Sslcommerz/sql/mysql4-install-1.1.0.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *
+ *
+ * @author Leton Miah (sslcommerz.com.bd)
+ * @copyright  Copyright (c) 2016 SSL WIRELESS
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();
